ef192d7444406dd2c71876b33cb13c22905b24ad
thiagobuenos <thiagoteofilo26@gmail.com>
2022-06-14 07:20:02 -0300

    chore: add repositories Category

---
+++ src/repositories/CategoryRepository.ts
@@ -0,0 +1,26 @@
+import { Category } from "../model/category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoryRepository {
+  private categories: Category[];
+  constructor() {
+    this.categories = [];
+  }
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoryRepository };

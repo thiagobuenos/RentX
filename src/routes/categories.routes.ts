5d879855d369cf0c461e1ee59a5f7ef9b93bcfb1
thiagobuenos <thiagoteofilo26@gmail.com>
2022-06-15 06:55:28 -0300

    chore: add validation name category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoryRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExits = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExits) {
+    return response.status(400).json({ error: "Category Already Exists" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();

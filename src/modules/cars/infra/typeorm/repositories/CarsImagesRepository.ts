9a6021c3c5c17884ad537e2ef09ab962034a8412
thiagobuenos <thiagoteofilo26@gmail.com>
2022-08-25 07:39:41 -0300

    chore: create entity and Repository CarImage

---
+++ src/modules/cars/infra/typeorm/repositories/CarsImagesRepository.ts
@@ -0,0 +1,26 @@
+import { Repository } from "typeorm";
+
+import { AppDataSource } from "../../../../../../dataSource";
+import { ICarsImagesRepository } from "../../../repositories/ICarsImagesRepository";
+import { CarImage } from "../entities/CarImage";
+
+class CarsImagesRepository implements ICarsImagesRepository {
+  private repository: Repository<CarImage>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(CarImage);
+  }
+
+  async create(car_id: string, image_name: string): Promise<CarImage> {
+    const carImage = await this.repository.create({
+      car_id,
+      image_name,
+    });
+
+    this.repository.save(carImage);
+
+    return carImage;
+  }
+}
+
+export { CarsImagesRepository };

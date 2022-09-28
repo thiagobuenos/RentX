8a6fc75e71a59e2ed6f75310b11504a6eefcfee4
thiagobuenos <thiagoteofilo26@gmail.com>
2022-09-28 20:58:39 -0300

    chore: add UserToken repository, DTO and Interfacerepository

---
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,29 @@
+import { Repository } from "typeorm";
+
+import { AppDataSource } from "../../../../../../dataSource";
+import { ICreateUserTokenDTO } from "../../../dtos/ICreateUsersTokensDTO";
+import { IUsersTokensRepository } from "../../../repositories/IUsersTokensRepository";
+import { UserTokens } from "../entities/UserTokens";
+
+class UsersTokensRepository implements IUsersTokensRepository {
+  private repository: Repository<UserTokens>;
+  constructor() {
+    this.repository = AppDataSource.getRepository(UserTokens);
+  }
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = this.repository.create({
+      expires_date,
+      refresh_token,
+      user_id,
+    });
+    await this.repository.save(userToken);
+
+    return userToken;
+  }
+}
+
+export { UsersTokensRepository };

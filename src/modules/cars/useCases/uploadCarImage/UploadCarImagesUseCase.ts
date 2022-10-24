5ea3da67d1d12d2803ed35dbf24471361c42e7aa
thiagobuenos <thiagoteofilo26@gmail.com>
2022-10-24 06:44:12 -0300

    chore: add upload carimage

--- src/modules/cars/useCases/uploadCarImage/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImagesUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { IStorageProvider } from "../../../../shared/container/providers/StorageProvider/IStorageProvider";
 import { CarsImagesRepository } from "../../infra/typeorm/repositories/CarsImagesRepository";
 
 interface IRequest {
@@ -11,12 +12,15 @@ interface IRequest {
 class UploadCarImagesUseCase {
   constructor(
     @inject("CarsImagesRepository")
-    private carsImagesRepository: CarsImagesRepository
+    private carsImagesRepository: CarsImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async (image) => {
       await this.carsImagesRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
